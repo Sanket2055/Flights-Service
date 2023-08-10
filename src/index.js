bd0f3be279904f8fce028ba769aa12d908cd466d
Sanket2055 <sanketdiwate95@gmail.com>
2023-08-11 02:35:16 +0530

    sequelize setup done ✅

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// ORM - Object Relational Mapper - a library that allows us to interact with a relational database using an object-oriented paradigm. This allows us to write JavaScript code to interact with a database instead of SQL queries.
+
+
 const { PORT } = require('./config/serverConfig');
+
+
 const setupAndStartServer = async () => {
     const app = express();
+
+    app.use(bodyParser.json());
+
+    //  tells Express to parse the body of requests that have a Content-Type header of application/json. This will convert the body of the request into a JavaScript object that can be accessed in the req.body property of the request object.
+
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    // tells Express to parse the body of requests that have a Content-Type header of application/x-www-form-urlencoded
 
     app.listen(PORT, () => {
         console.log('Server is running on port ' + PORT);
